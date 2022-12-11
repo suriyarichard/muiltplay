bfa76edc8792b40656c91ea2a956f8618ebc08b4
suriyaprakash <75439259+suriyarichard@users.noreply.github.com>
2022-12-11 17:43:20 +0530

    Update allGames.dart

--- lib/screens/allGames.dart
+++ lib/screens/allGames.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
 import 'package:muiltplay/widgets/gameCard.dart';
 
 class Allgames extends StatefulWidget {
@@ -43,6 +44,40 @@ class _AllgamesState extends State<Allgames> {
             SizedBox(
               height: 20,
             ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.network(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGBs5zBdULlvWEJXCgMbl35byCAlOqi5w11UJAjLAs&s",
+                // height: 150.0,
+                // width: 300.0,
+              ),
+            ),
+            // Container(
+            //   decoration: BoxDecoration(
+            //     borderRadius: BorderRadius.circular(8.0),
+            //   ),
+            //   child: Image.network(
+            //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGBs5zBdULlvWEJXCgMbl35byCAlOqi5w11UJAjLAs&s",
+            //     // height: 150.0,
+            //     // width: 100.0,
+            //   ),
+            //   //   // margin: const EdgeInsets.symmetric(horizontal: 16.0),
+            //   //   height: 300,
+            //   //   // width: 200,
+            //   //   decoration: BoxDecoration(
+            //   //     borderRadius: BorderRadius.circular(40),
+            //   //     // image: const DecorationImage(
+            //   //     //     image: NetworkImage(
+            //   //     //         "https://assets8.lottiefiles.com/packages/lf20_V53nC8.json"),
+            //   //     //     fit: BoxFit.cover
+            //   //     //     ),
+            //   //   ),
+            //   //   child: Lottie.network(
+            //   //       "https://assets8.lottiefiles.com/packages/lf20_V53nC8.json"),
+            // ),
+            SizedBox(
+              height: 20,
+            ),
             Padding(
               padding: const EdgeInsets.all(10.0),
               child: Row(
@@ -74,15 +109,15 @@ class _AllgamesState extends State<Allgames> {
                     icon:
                         'https://assets3.lottiefiles.com/packages/lf20_7ehddyj8.json',
                     title: 'Tic Tac Toc',
-                    color: Colors.indigoAccent,
-                    fontColor: Colors.white,
+                    color: Colors.white,
+                    fontColor: Colors.grey,
                   ),
                   gameCard(
                     icon:
                         'https://assets3.lottiefiles.com/packages/lf20_7ehddyj8.json',
                     title: 'ENTERTAINMENT',
-                    color: Colors.indigoAccent,
-                    fontColor: Colors.white,
+                    color: Colors.white,
+                    fontColor: Colors.grey,
                   ),
                 ],
               ),
