e3d7f4ef0520ba306778bd52e7be6ea2df114cd5
suriyaprakash <75439259+suriyarichard@users.noreply.github.com>
2022-12-11 10:47:40 +0530

    Initial commit

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Icon(Icons.grid_3x3),
+        
+      ],
+    ));
+  }
+}
